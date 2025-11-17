cdfd4aee4a881cc5277653547fc5ac48aa7fa68c
orillad <orillad2003@gmail.com>
2025-11-17 17:19:52 +0100

    test

--- scheduler.c
+++ scheduler.c
@@ -6,80 +6,95 @@
 #include "queue.h"
 #include "scheduler.h"
 
-int num_algorithms() {
-  return sizeof(algorithmsNames) / sizeof(char *);
+int num_algorithms()
+{
+    return sizeof(algorithmsNames) / sizeof(char *);
 }
 
-int num_modalities() {
-  return sizeof(modalitiesNames) / sizeof(char *);
+int num_modalities()
+{
+    return sizeof(modalitiesNames) / sizeof(char *);
 }
 
-size_t initFromCSVFile(char* filename, Process** procTable){
-    FILE* f = fopen(filename,"r");
-    
+size_t initFromCSVFile(char *filename, Process **procTable)
+{
+    FILE *f = fopen(filename, "r");
+
     size_t procTableSize = 10;
-    
-    *procTable = malloc(procTableSize * sizeof(Process));
-    Process * _procTable = *procTable;
 
-    if(f == NULL){
-      perror("initFromCSVFile():::Error Opening File:::");   
-      exit(1);             
+    *procTable = malloc(procTableSize * sizeof(Process));
+    Process *_procTable = *procTable;
+
+    if (f == NULL)
+    {
+        perror("initFromCSVFile():::Error Opening File:::");
+        exit(1);
     }
 
-    char* line = NULL;
+    char *line = NULL;
     size_t buffer_size = 0;
-    size_t nprocs= 0;
-    while( getline(&line,&buffer_size,f)!=-1){
-        if(line != NULL){
-            Process p = initProcessFromTokens(line,";");
+    size_t nprocs = 0;
+    while (getline(&line, &buffer_size, f) != -1)
+    {
+        if (line != NULL)
+        {
+            Process p = initProcessFromTokens(line, ";");
 
-            if (nprocs==procTableSize-1){
-                procTableSize=procTableSize+procTableSize;
-                _procTable=realloc(_procTable, procTableSize * sizeof(Process));
+            if (nprocs == procTableSize - 1)
+            {
+                procTableSize = procTableSize + procTableSize;
+                _procTable = realloc(_procTable, procTableSize * sizeof(Process));
             }
 
-            _procTable[nprocs]=p;
+            _procTable[nprocs] = p;
 
             nprocs++;
         }
     }
-   free(line);
-   fclose(f);
-   return nprocs;
+    free(line);
+    fclose(f);
+    return nprocs;
 }
 
-size_t getTotalCPU(Process *procTable, size_t nprocs){
-    size_t total=0;
-    for (int p=0; p<nprocs; p++ ){
-        total += (size_t) procTable[p].burst;
+size_t getTotalCPU(Process *procTable, size_t nprocs)
+{
+    size_t total = 0;
+    for (int p = 0; p < nprocs; p++)
+    {
+        total += (size_t)procTable[p].burst;
     }
     return total;
 }
 
-int getCurrentBurst(Process* proc, int current_time){
+int getCurrentBurst(Process *proc, int current_time)
+{
     int burst = 0;
-    for(int t=0; t<current_time; t++){
-        if(proc->lifecycle[t] == Running){
+    for (int t = 0; t < current_time; t++)
+    {
+        if (proc->lifecycle[t] == Running)
+        {
             burst++;
         }
     }
     return burst;
 }
 
-int run_dispatcher(Process *procTable, size_t nprocs, int algorithm, int modality, int quantum){
+int run_dispatcher(Process *procTable, size_t nprocs, int algorithm, int modality, int quantum)
+{
 
-    Process * _proclist;
+    Process *_proclist;
 
-    qsort(procTable,nprocs,sizeof(Process),compareArrival);
+    qsort(procTable, nprocs, sizeof(Process), compareArrival);
 
     init_queue();
-    size_t duration = getTotalCPU(procTable, nprocs) +1;
+    size_t duration = getTotalCPU(procTable, nprocs) + 1;
 
-    for (int p=0; p<nprocs; p++ ){
-        procTable[p].lifecycle = malloc( duration * sizeof(int));
-        for(int t=0; t<duration; t++){
-            procTable[p].lifecycle[t]=-1;
+    for (int p = 0; p < nprocs; p++)
+    {
+        procTable[p].lifecycle = malloc(duration * sizeof(int));
+        for (int t = 0; t < duration; t++)
+        {
+            procTable[p].lifecycle[t] = -1;
         }
         procTable[p].waiting_time = 0;
         procTable[p].return_time = 0;
@@ -87,81 +102,101 @@ int run_dispatcher(Process *procTable, size_t nprocs, int algorithm, int modalit
         procTable[p].completed = false;
     }
 
-    printSimulation(nprocs,procTable,duration);
+    Process *current = NULL;
 
-    for (int p=0; p<nprocs; p++ ){
+    for (int t = 0; t < duration; t++)
+    {
+
+        // per cada unitat de temps afegit a la cua els processos que arriben
+        for (int p = 0; p < nprocs; p++)
+        {
+            // comparem si el temps es el mateix que el del process
+            if (procTable[p].arrive_time == t)
+            {
+                enqueue(&procTable[p]);
+            }
+        }
+
+        if ()
+    }
+
+    printSimulation(nprocs, procTable, duration);
+
+    for (int p = 0; p < nprocs; p++)
+    {
         destroyProcess(procTable[p]);
     }
 
     cleanQueue();
     return EXIT_SUCCESS;
-
 }
 
+void printSimulation(size_t nprocs, Process *procTable, size_t duration)
+{
 
-void printSimulation(size_t nprocs, Process *procTable, size_t duration){
-
-    printf("%14s","== SIMULATION ");
-    for (int t=0; t<duration; t++ ){
-        printf("%5s","=====");
+    printf("%14s", "== SIMULATION ");
+    for (int t = 0; t < duration; t++)
+    {
+        printf("%5s", "=====");
     }
     printf("\n");
 
-    printf ("|%4s", "name");
-    for(int t=0; t<duration; t++){
-        printf ("|%2d", t);
+    printf("|%4s", "name");
+    for (int t = 0; t < duration; t++)
+    {
+        printf("|%2d", t);
     }
-    printf ("|\n");
+    printf("|\n");
 
-    for (int p=0; p<nprocs; p++ ){
+    for (int p = 0; p < nprocs; p++)
+    {
         Process current = procTable[p];
-            printf ("|%4s", current.name);
-            for(int t=0; t<duration; t++){
-                printf("|%2s",  (current.lifecycle[t]==Running ? "E" : 
-                        current.lifecycle[t]==Bloqued ? "B" :   
-                        current.lifecycle[t]==Finished ? "F" : " "));
-            }
-            printf ("|\n");
-        
+        printf("|%4s", current.name);
+        for (int t = 0; t < duration; t++)
+        {
+            printf("|%2s", (current.lifecycle[t] == Running ? "E" : current.lifecycle[t] == Bloqued ? "B"
+                                                                : current.lifecycle[t] == Finished  ? "F"
+                                                                                                    : " "));
+        }
+        printf("|\n");
     }
-
-
 }
 
-void printMetrics(size_t simulationCPUTime, size_t nprocs, Process *procTable ){
+void printMetrics(size_t simulationCPUTime, size_t nprocs, Process *procTable)
+{
 
-    printf("%-14s","== METRICS ");
-    for (int t=0; t<simulationCPUTime+1; t++ ){
-        printf("%5s","=====");
+    printf("%-14s", "== METRICS ");
+    for (int t = 0; t < simulationCPUTime + 1; t++)
+    {
+        printf("%5s", "=====");
     }
     printf("\n");
 
-    printf("= Duration: %ld\n", simulationCPUTime );
-    printf("= Processes: %ld\n", nprocs );
+    printf("= Duration: %ld\n", simulationCPUTime);
+    printf("= Processes: %ld\n", nprocs);
 
     size_t baselineCPUTime = getTotalCPU(procTable, nprocs);
-    double throughput = (double) nprocs / (double) simulationCPUTime;
-    double cpu_usage = (double) simulationCPUTime / (double) baselineCPUTime;
+    double throughput = (double)nprocs / (double)simulationCPUTime;
+    double cpu_usage = (double)simulationCPUTime / (double)baselineCPUTime;
 
-    printf("= CPU (Usage): %lf\n", cpu_usage*100 );
-    printf("= Throughput: %lf\n", throughput*100 );
+    printf("= CPU (Usage): %lf\n", cpu_usage * 100);
+    printf("= Throughput: %lf\n", throughput * 100);
 
     double averageWaitingTime = 0;
     double averageResponseTime = 0;
     double averageReturnTime = 0;
     double averageReturnTimeN = 0;
 
-    for (int p=0; p<nprocs; p++ ){
-            averageWaitingTime += procTable[p].waiting_time;
-            averageResponseTime += procTable[p].response_time;
-            averageReturnTime += procTable[p].return_time;
-            averageReturnTimeN += procTable[p].return_time / (double) procTable[p].burst;
+    for (int p = 0; p < nprocs; p++)
+    {
+        averageWaitingTime += procTable[p].waiting_time;
+        averageResponseTime += procTable[p].response_time;
+        averageReturnTime += procTable[p].return_time;
+        averageReturnTimeN += procTable[p].return_time / (double)procTable[p].burst;
     }
 
-
-    printf("= averageWaitingTime: %lf\n", (averageWaitingTime/(double) nprocs) );
-    printf("= averageResponseTime: %lf\n", (averageResponseTime/(double) nprocs) );
-    printf("= averageReturnTimeN: %lf\n", (averageReturnTimeN/(double) nprocs) );
-    printf("= averageReturnTime: %lf\n", (averageReturnTime/(double) nprocs) );
-
+    printf("= averageWaitingTime: %lf\n", (averageWaitingTime / (double)nprocs));
+    printf("= averageResponseTime: %lf\n", (averageResponseTime / (double)nprocs));
+    printf("= averageReturnTimeN: %lf\n", (averageReturnTimeN / (double)nprocs));
+    printf("= averageReturnTime: %lf\n", (averageReturnTime / (double)nprocs));
 }
